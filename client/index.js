44c0d7fd6cbc524bb151b154ffff5f0b43a50a63
MichalGilead <michaligld@gmail.com>
2019-12-18 20:55:42 +0200

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,9 @@
+
+// client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
