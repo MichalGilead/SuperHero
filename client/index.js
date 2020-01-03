177608cf0f2c4b5bf893b02179384d7fd5fb1c5e
MichalGilead <michaligld@gmail.com>
2020-01-03 12:56:03 +0200

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,8 +2,9 @@
 // client/src/index.js
 
 import React from 'react';
+//import {Link} from "react-router-dom";
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
